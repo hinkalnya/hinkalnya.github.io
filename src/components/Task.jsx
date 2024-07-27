35ada6931244e96cfd3fbbc94b5db27157187b99
hinkalnya <176493240+hinkalnya@users.noreply.github.com>
2024-07-27 21:00:15 +0300

    Changed normal function to arrow for consistency

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,6 +1,4 @@
-import React from "react"
-
-function Task({ title, description, price }) {
+const Task = ({ title, description, price }) => {
     return (
         <div className="task">
             <div className="task-title text-lg mb-1">{title}</div>
